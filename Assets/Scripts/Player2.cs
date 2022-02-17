7212f8209f4489eae8b3667187ea3b6202bd735f
China17524 <60266047.okura.cgp@gmail.com>
2022-02-17 14:09:52 +0900

    2022/02/17-2
    挙動の改善に成功

--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -6,24 +6,12 @@ public class Player2 : MonoBehaviour
 {
     //プレイヤーの移動する速さ
     public float move_speed = 15;
-
-    //プレイヤーの回転する速さ
-    public float rotate_speed = 5;
-
-    //プレイヤーの回転する向き
-    //1 -> （プレイヤーから見て）時計回り
-    //-1 -> （プレイヤーから見て）反時計回り
-    private int rotate_direction = 0;
-
     //プレイヤーのRigidbody
     private Rigidbody Rig = null;
-
     //地面に着地しているか判定する変数
     public bool Grounded;
-
     //ジャンプ力
     public float Jumppower;
-
 
     void Start()
     {
@@ -36,12 +24,14 @@ public class Player2 : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {
-        Horizontal_Rotate();
+    { 
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 1, 1)).normalized;
+        Vector3 cameraRight = Vector3.Scale(Camera.main.transform.right, new Vector3(1, 1, 1)).normalized;
+        Vector3 moveForward = cameraForward * Input.GetAxis("Vertical") + cameraRight * Input.GetAxis("Horizontal");
 
-        Vector3 move_direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        Rig.velocity = moveForward * move_speed + new Vector3(0, Rig.velocity.y);
 
-        Rig.MovePosition(Rig.position + transform.TransformDirection(move_direction) * move_speed * Time.deltaTime);
+        if (moveForward.magnitude != 0f) transform.rotation = Quaternion.LookRotation(moveForward);
     }
 
     void Jump()
@@ -62,28 +52,5 @@ public class Player2 : MonoBehaviour
         {
             Grounded = true;//  Groundedをtrueにする
         }
-    }
-
-    void Horizontal_Rotate()
-    {
-        if (Input.GetKey(KeyCode.Q))
-        {
-            rotate_direction = -1;
-        }
-        else if (Input.GetKey(KeyCode.E))
-        {
-            rotate_direction = 1;
-        }
-        else
-        {
-            rotate_direction = 0;
-        }
-
-        // オブジェクトからみて垂直方向を軸として回転させるQuaternionを作成
-        Quaternion rot = Quaternion.AngleAxis(rotate_direction * rotate_speed, transform.up);
-        // 現在の自信の回転の情報を取得する。
-        Quaternion q = this.transform.rotation;
-        // 合成して、自身に設定
-        this.transform.rotation = rot * q;
     }
 }
