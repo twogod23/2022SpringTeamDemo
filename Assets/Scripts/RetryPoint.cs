8f0340c58f2c0d18d590323f4c3b75f17ef77b8a
China17524 <60266047.okura.cgp@gmail.com>
2022-02-20 15:32:52 +0900

    2022/02/20-2
    ライトの追加
    ステージ修正
    ビルド

---
+++ Assets/Scripts/RetryPoint.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetryPoint : MonoBehaviour
+{
+    private Stack<Transform> _retryPointStack = new Stack<Transform>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //リトライエリアをスタックに追加
+        if (other.gameObject.layer == LayerMask.NameToLayer("RetryPoint"))
+        {
+            _retryPointStack.Push(other.gameObject.transform);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //敵に接触
+        if (other.gameObject.layer == LayerMask.NameToLayer("Miss"))
+        {
+            //座標を戻す
+            this.gameObject.transform.position = _retryPointStack.Peek().position;
+
+            //その他、フェードや諸々の設定を元に戻すなどのリトライ処理
+            //元に戻したい処理が増えてくるなら、
+            //Destroyして、Instantiateするってのもありかも
+        }
+    }
+}
